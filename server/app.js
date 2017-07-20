470744ee446f22bda717f052242e903a8996e899
Todd <todd@grooveio.com>
2017-07-20 08:00:08 -0700

    init

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 5000);
+  res.status(err.status || 3000);
   res.render('error');
 });
 
